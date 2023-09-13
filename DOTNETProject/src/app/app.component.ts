30dec1eb268f2aa6205806acf22ffd2668fb9585
Dylan Cheong Nam Kwong <dylancnk_97@hotmail.com>
2023-09-13 16:08:06 -0400

    Installed Bootstrap & some html code

--- DOTNETProject/src/app/app.component.ts
+++ DOTNETProject/src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+// import { LoginSignupService } from "./login-signup.service";
+import { Router } from "@angular/router";
+// import { NotifyService } from './notify.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'DOTNETProject';
+  constructor(private router: Router, ) {
+    
+  }
+
+  userObj: any;
+  // subscription: Subscription;
+
+  ngOnInit() {
+    this.checkLoginStatusFunc();
+  }
+
+  logoutFunc() {
+    // if (confirm("Are you sure, you want to logout ?")) {
+    //   let reply = this.loginSignup.logoutUserFunc();
+    //   reply.then(
+    //     (iData: any) => {
+    //       this.checkLoginStatusFunc();
+    //       this.router.navigate(['login']);
+    //     }
+    //   )
+    // }
+  }
+
+  checkLoginStatusFunc() {
+  //   let userStatus = this.loginSignup.loggedInUserDataFunc();
+  //   if (userStatus) {
+  //     this.userObj = JSON.parse(userStatus);
+  //   }
+  //   else {
+  //     this.userObj = null;
+  //   }
+  // }
+}
 }
