7bbb96742bea8edcd1a4f3e7650f65fe711224e8
Dylan Cheong Nam Kwong <dylancnk_97@hotmail.com>
2023-09-14 19:14:18 -0400

    .NETProject-Created new component Employers

--- DOTNETProject/src/app/app.module.ts
+++ DOTNETProject/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployersComponent } from './employers/employers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployersComponent
   ],
   imports: [
     BrowserModule
